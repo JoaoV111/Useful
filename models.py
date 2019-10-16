20b46f947f8d482b2b506a6c3062218c5159e0bf
JoaoV111 <52671234+JoaoV111@users.noreply.github.com>
2019-10-15 22:00:34 -0300

    Add files via upload

---
+++ models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.contrib import admin
+from django.contrib.auth.models import AbstractUser
+import datetime
+
+class Base (models.Model):
+    created_at = models.DateTimeField(default=datetime.date.today)
+    updated_at = models.DateTimeField(default=datetime.date.today)
+    deleted_at = models.DateTimeField(default=datetime.date.today)
+    class Meta:
+        abstract = True
+
+class Writer (Base):
+    pass
+
+class Article (Base):
+    writer_id = models.ForeignKey(Writer, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=200)
+    publish_date = models.DateTimeField(default=datetime.date.today)
+
+    def __str__(self):
+        return self.title
+
+class Element (Base):
+    Article_id = models.ForeignKey(Article, on_delete=models.CASCADE)
+    class Meta:
+        abstract = True
+
+class MidiaVisual (Element):
+    src = models.CharField(max_length=200)
+    alt = models.CharField(max_length=200)
+    width = models.IntegerField(default = 0)
+    height = models.IntegerField(default = 0)
+    srs_link = models.IntegerField(max_length=200)
+    
+    def __str__(self):
+        return self.alt
+
+class Paragraph (Element):
+    text = models.CharField(max_length=5000)
+    Article_ID = models.IntegerField(default = 0)
+
+class YouTube (Element):
+    src = models.CharField(max_length=200)
